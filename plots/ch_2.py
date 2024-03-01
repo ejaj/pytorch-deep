8ed43a66f36415c7e14f83d9b981914ed02ee2c0
Kazi Ejajul <kejaj.777@gmail.com>
2024-03-01 20:51:16 +0100

    Training Loop

--- plots/ch_2.py
+++ plots/ch_2.py
@@ -39,3 +39,15 @@ def plot_resumed_losses(saved_epoch, saved_losses, saved_val_losses, n_epochs, l
     plt.legend()
     plt.tight_layout()
     return fig
+
+
+def figure1(x, y):
+    fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+
+    ax.scatter(x, y)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_ylim([0, 3.1])
+    ax.set_title('Generated Data - Full Dataset')
+    fig.tight_layout()
+    return fig
