fb861bc70fe0db9c02508960a753f284395c68ee
Kazi Ejajul <kejaj.777@gmail.com>
2024-03-16 10:31:38 +0100

    Updated stepbystep class v2

--- stepbystep/v2.py
+++ stepbystep/v2.py
@@ -7,10 +7,15 @@ import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 
 plt.style.use('fivethirtyeight')
+from IPython.display import display
 
 
 class StepByStep(object):
     def __init__(self, model, loss_fn, optimizer):
+        # Here we define the attributes of our class
+
+        # We start by storing the arguments as attributes
+        # to use them later
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
@@ -18,18 +23,26 @@ class StepByStep(object):
         # Let's send the model to the specified device right away
         self.model.to(self.device)
 
+        # These attributes are defined here, but since they are
+        # not informed at the moment of creation, we keep them None
         self.train_loader = None
         self.val_loader = None
         self.writer = None
 
+        # These attributes are going to be computed internally
         self.losses = []
         self.val_losses = []
-        self.total_epochs = []
+        self.total_epochs = 0
 
         self.visualization = {}
         self.handles = {}
 
+        # Creates the train_step function for our model,
+        # loss function and optimizer
+        # Note: there are NO ARGS there! It makes use of the class
+        # attributes directly
         self.train_step_fn = self._make_train_step_fn()
+        # Creates the val_step function for our model and loss
         self.val_step_fn = self._make_val_step_fn()
 
     def to(self, device):
@@ -38,7 +51,7 @@ class StepByStep(object):
         # the mini-batches) and sends the model to the device
         try:
             self.device = device
-            self.model.to(device)
+            self.model.to(self.device)
         except RuntimeError:
             self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
             print(f"Couldn't send it to {device}, sending it to {self.device} instead.")
@@ -47,6 +60,7 @@ class StepByStep(object):
     def set_loaders(self, train_loader, val_loader=None):
         # This method allows the user to define which train_loader (and val_loader, optionally) to use
         # Both loaders are then assigned to attributes of the class
+        # So they can be referred to later
         self.train_loader = train_loader
         self.val_loader = val_loader
 
@@ -58,6 +72,7 @@ class StepByStep(object):
     def _make_train_step_fn(self):
         # This method does not need ARGS... it can refer to
         # the attributes: self.model, self.loss_fn and self.optimizer
+
         # Builds function that performs a step in the train loop
         def perform_train_step_fn(x, y):
             # Sets model to TRAIN mode
@@ -65,33 +80,30 @@ class StepByStep(object):
 
             # Step 1 - Computes our model's predicted output - forward pass
             yhat = self.model(x)
-
             # Step 2 - Computes the loss
             loss = self.loss_fn(yhat, y)
-
             # Step 3 - Computes gradients for both "a" and "b" parameters
             loss.backward()
-
             # Step 4 - Updates parameters using gradients and the learning rate
             self.optimizer.step()
-            self.optimizer().zero_grad()
+            self.optimizer.zero_grad()
 
-            # Return the loss
+            # Returns the loss
             return loss.item()
 
+        # Returns the function that will be called inside the train loop
         return perform_train_step_fn
 
     def _make_val_step_fn(self):
+        # Builds function that performs a step in the validation loop
         def perform_val_step_fn(x, y):
             # Sets model to EVAL mode
             self.model.eval()
 
             # Step 1 - Computes our model's predicted output - forward pass
             yhat = self.model(x)
-
             # Step 2 - Computes the loss
             loss = self.loss_fn(yhat, y)
-
             # There is no need to compute Steps 3 and 4, since we don't update parameters during evaluation
             return loss.item()
 
@@ -111,8 +123,12 @@ class StepByStep(object):
         if data_loader is None:
             return None
 
+        # Once the data loader and step function, this is the same
+        # mini-batch loop we had before
         mini_batch_losses = []
         for x_batch, y_batch in data_loader:
+            display(x_batch.shape, y_batch.shape)
+            break  # Just to check the first batch
             x_batch = x_batch.to(self.device)
             y_batch = y_batch.to(self.device)
 
@@ -141,17 +157,20 @@ class StepByStep(object):
             # Keeps track of the numbers of epochs
             # by updating the corresponding attribute
             self.total_epochs += 1
+
             # inner loop
             # Performs training using mini-batches
             loss = self._mini_batch(validation=False)
             self.losses.append(loss)
+
             # VALIDATION
             # no gradients in validation!
             with torch.no_grad():
                 # Performs evaluation using mini-batches
                 val_loss = self._mini_batch(validation=True)
                 self.val_losses.append(val_loss)
-            # If a SummaryWriter has been set
+
+            # If a SummaryWriter has been set...
             if self.writer:
                 scalars = {'training': loss}
                 if val_loss is not None:
@@ -160,22 +179,25 @@ class StepByStep(object):
                 self.writer.add_scalars(main_tag='loss',
                                         tag_scalar_dict=scalars,
                                         global_step=epoch)
+
         if self.writer:
+            # Closes the writer
             self.writer.close()
 
     def save_checkpoint(self, filename):
         # Builds dictionary with all elements for resuming training
-        checkpoint = {
-            'epoch': self.total_epochs,
-            'model_state_dict': self.model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict(),
-            'loss': self.losses,
-            'val_loss': self.val_losses
-        }
+        checkpoint = {'epoch': self.total_epochs,
+                      'model_state_dict': self.model.state_dict(),
+                      'optimizer_state_dict': self.optimizer.state_dict(),
+                      'loss': self.losses,
+                      'val_loss': self.val_losses}
+
         torch.save(checkpoint, filename)
 
     def load_checkpoint(self, filename):
+        # Loads dictionary
         checkpoint = torch.load(filename)
+
         # Restore state for model and optimizer
         self.model.load_state_dict(checkpoint['model_state_dict'])
         self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
